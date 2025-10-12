61594bb4601c36edbf89908140508fd0801e3809
Sarah <sarahyukinonakada@gmail.com>
2025-10-12 17:08:01 -0300

    install bootstrap and react router

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from "react";
+import { Button } from "react-bootstrap";
+
+function Home(): JSX.Element {
+  return (
+    <>
+        <Button>HEllo World</Button>
+    </>
+  )
+}
+
+export default Home;
