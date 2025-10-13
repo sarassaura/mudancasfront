f58873e6452e7d4b95afde527ccba771e3ef27e3
Silvia Abe <silviaabebuzatto@gmail.com>
2025-10-13 18:39:20 -0300

    add logo

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,18 +2,26 @@ import type { JSX } from "react";
 import { Container, Dropdown, Nav, Navbar } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/Logo.svg';
 
 function Layout(): JSX.Element {
   return (
     <>
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar bg="light" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand as={Link} to="/">Logo</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src={Logo} 
+                        alt="Logo da Empresa"
+                        height="50" 
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/cadastrar">Cadastrar</Nav.Link>
-                    <Nav.Link as={Link} to="/pedidos-mudanca">Pedidos de Mundança</Nav.Link>
-                    <Nav.Link as={Link} to="/premiacoes">Premiações</Nav.Link>
+                    <Nav.Link className="text-dark" as={Link} to="/">Início</Nav.Link>
+                    <Nav.Link className="text-dark" as={Link} to="/cadastrar">Cadastrar</Nav.Link>
+                    <Nav.Link className="text-dark" as={Link} to="/pedidos-mudanca">Pedidos de Mundança</Nav.Link>
+                    <Nav.Link className="text-dark" as={Link} to="/premiacoes">Premiações</Nav.Link>
                 </Nav>
                 <Nav>
                     <Dropdown drop="down-centered">
